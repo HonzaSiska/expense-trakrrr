ac5b881689dcc0e8129f70eb7b5f01886b1fe4c3
HonzaSiska <siskajan@hotmail.com>
2022-08-19 14:55:19 +0200

    refactored add data by adding useFirestore hook, created useCollection, fetched data on dashboard

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,69 +1,66 @@
 import { useAuthContext } from '../../hooks/useAuthContext'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { db } from '../../firebase/config'
 import { collection, addDoc } from 'firebase/firestore'
+import { useFirestore } from '../../hooks/useFirestore'
 
 //styles
 import './Create.css'
 
 export default function Create() {
   const { user,translate, lang } = useAuthContext()
+  const { addDocument, response } = useFirestore('expenses')
+
+  // Destructuring respone
+  const { error, isPending, success, document } = response
 
   const [ description, setDescription ] = useState('')
   const [date, setDate] = useState('')
   const [amount, setAmount] = useState(0)
-  const [error, setError] = useState(null)
-  const [isPending, setIsPending] = useState(false)
-  // console.log(user.uid)
-  // console.log({
-  //   description,
-  //   date,
-  //   amount,
-  //   user: user.uid
-  // })
+  const [validationError, setValidationError] = useState(null)
 
+  
+  useEffect(()=> {
+    const message = translate(lang, 'Nový záznam byl vytvořen ','New expense was created','Nuevo gasto fue creado')
+        
+    setValidationError(message)
+        setTimeout(() => {
+          setValidationError('')
+        }, 2500);
+
+        setDate('')
+        setAmount('')
+        setDescription('')
+
+  }, [success])
+
+  //ADD Expense
   const handleSubmit = async(e) => {
       e.preventDefault()
-      setError(null)
+      setValidationError(null)
       if(description ===''){
         const message = translate(lang, 'Vyplň pole popis','Fill out description','Complete la descripción')
-        setError(message)
+        setValidationError(message)
         return
       }
       if(!date){
         const message = translate(lang, 'Vyplň pole datum','Fill out date','Complete la fecha')
-        setError(message)
+        setValidationError(message)
         return
       }
       if(!amount || amount === 0){
         const message = translate(lang, 'Vyplň pole částka','Fill out amount','Complete la monto')
-        setError(message)
+        setValidationError(message)
         return
       }
       const ref = collection(db, 'expenses')
 
-      try {
-        await addDoc(ref,{
-          description,
-          date: new Date(date),
-          amount,
-          user: user.uid
-        })
-
-        setDate('')
-        setAmount('')
-        
-        const message = translate(lang, 'Nový záznam byl vytvořen ','New expense was created','Nuevo gasto fue creado')
-        setError(message)
-        setTimeout(() => {
-          setError('')
-        }, 2500);
-
-      } catch (err) {
-        const message = translate(lang, 'Nový záznam NEBYL vytvořen....error','New expense was NOT created ...error','Nuevo gasto NO fue creado ...error')
-        setError(message)
-      }
-      
+      await addDocument({
+        description,
+        date: new Date(date),
+        amount: parseFloat(amount),
+        user: user.uid
+      })
 
   }
   return (
@@ -98,6 +95,7 @@ export default function Create() {
                     />
                 </label>
                 
+                { validationError && <p className='error'>{validationError}</p>}
                 { error && <p className='error'>{error}</p>}
                 {isPending ? <p>{translate(lang, 'ukládá se ...','processing ...','procesamiento ...')}</p> : <button className='btn'>{translate(lang, 'Přihlásit se','Send','Iniciar sesión')}</button>}
             </form>
--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,10 +1,25 @@
+import { useCollection } from '../../hooks/useCollection'
+import { useAuthContext } from '../../hooks/useAuthContext'
+
+//components
+import DasboardList from './DashboardList'
+
 //styles
 import './Dashboard.css'
 
 export default function Dashboard() {
+  const { user } = useAuthContext()
+  const { documents, error } = useCollection('expenses', ["uid", "==", user.uid],
+  //  [null]
+   ["createdAt", "desc"])
+  
   return (
     <div className='dashboard'>
-        <h2 className='pageTitle'>Dashboard</h2>
+    <h2 className='pageTitle'>Dashboard</h2>
+      <div className='dashboard-items'>
+        <DasboardList documents={documents} />
+      </div>
+        
     </div>
   )
 }
