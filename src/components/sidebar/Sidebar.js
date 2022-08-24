43b31c46f291291f3a61a6839b20ca910ccc9c64
HonzaSiska <siskajan@hotmail.com>
2022-08-24 23:04:57 +0200

    added slide in menu animation

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,21 +1,37 @@
+import { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import { useAuthContext } from '../../hooks/useAuthContext'
 
 //components
 import Avatar from '../avatar/Avatar'
+import Menu from './../../assets/menu.svg'
+import Close from './../../assets/close.svg'
+
 
 //styles
 import './Sidebar.css'
 
 export default function Sidebar() {
   const { user, lang, translate  }  = useAuthContext()
+  const [ isOpen, setIsOpen ] = useState(false)
 
+  const openMenu = () => {
+    setIsOpen(true)
+  }
+  const closeMenu = () => {
+    setIsOpen(false)
+  }
   return (
-    <div className='sidebar'>
+    <div className={isOpen ? 'sidebar slide-menu-in': 'sidebar'}>
         <div className="sidebar-content">
+            <div className='open-menu'>
+                {!isOpen && <img onClick={openMenu} src={Menu} alt='menu-icon'/>}
+                {isOpen && <img className="close-btn" onClick={closeMenu} src={Close} alt='close'/>}
+            </div>
             
             <nav className="links">
                 <ul>
+                    
                     <li className='user'>
                         <Avatar url={user.photoURL}/>
                         <p>{user.displayName}</p>
